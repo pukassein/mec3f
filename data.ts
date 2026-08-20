d33a4c453e0b7e8bf89cb4465e97a6be0ed815aa
pukaLinu <moussahussein757@gmail.com>
2026-08-20 07:30:47 -0300

    edson

--- data.ts
+++ data.ts
@@ -245,6 +245,7 @@ export const ORGANIZATION_LOCAL: TeamMember[] = [
   { name: "Anna Beatriz Mota de Moura", institution: "" },
   { name: "Cristian Godoy", institution: "" },
   { name: "Diego David Pinzon Moreno", institution: "" },
+  { name: "Edson Massayuki Kakuno", institution: "" },
   { name: "Esdras Rebecchi de Almeida", institution: "" },
   { name: "Flávio Camacho", institution: "" },
   { name: "Gricelda Adelina Diaz Rolón", institution: "" },
